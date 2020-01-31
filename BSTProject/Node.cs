fed8976b42746607d489ce822c5ee1a411884f34
unknown <hjimmy0803@gmail.com>
2020-01-31 08:08:39 -0600

    Created my two classes and and variables.

---
+++ BSTProject/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSTProject
+{
+    class Node
+    {
+        public int data;
+        public Node leftChild;
+        public Node rightChild;
+    }
+}
